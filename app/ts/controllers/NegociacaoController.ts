0c23bd67056225e2eb86f92e2010a8bdd0fd5099
Eder Coloma <ecoloma@atechsp000160lx.atech.local.br>
2021-04-19 12:31:12 -0300

    Third class

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,12 +2,14 @@ class NegociacaoController{
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
-    private _negociacoes: Negociacoes;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView("#negociacoesView");
 
     constructor(){
         this._inputData = <HTMLInputElement> document.querySelector("#data");
         this._inputQuantidade = <HTMLInputElement> document.querySelector("#quantidade");
         this._inputValor = <HTMLInputElement> document.querySelector("#valor");
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event){
@@ -19,6 +21,7 @@ class NegociacaoController{
         );
 
         this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
     }
 
 }
